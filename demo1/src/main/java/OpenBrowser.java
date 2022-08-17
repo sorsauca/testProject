e9d5424cf4463f44072664c45c6a233097779ea0
Sorina Sauca <sorina.sauca@fortech.ro>
2022-08-17 12:24:19 +0300

    changes to first test

--- demo1/src/main/java/OpenBrowser.java
+++ demo1/src/main/java/OpenBrowser.java
@@ -1,5 +1,7 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -7,6 +9,7 @@ import org.testng.annotations.Test;
 public class OpenBrowser {
     private WebDriver driver;
     private String link = "https://qa-practice.netlify.app/";
+    private By menuAlerts = By.id("alerts");
 
     public WebDriver chromeInit (){
         WebDriverManager.chromedriver().setup();
@@ -27,6 +30,10 @@ public class OpenBrowser {
     public void test1 (){
         chromeInit();
         driver.get(link);
+        driver.manage().window().maximize();
+        WebElement alert = driver.findElement(menuAlerts);
+        alert.click();
+        driver.quit();
 
     }
 }
